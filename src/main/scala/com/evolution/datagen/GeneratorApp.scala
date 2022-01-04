d2d597d4309d34f077cacc12dcd27dc2f27c1cb6
Lev Khacheresiantc <lev_khacheresiantc@epam.com>
2022-01-04 04:58:55 +0300

    Added persistence of data to file system.

--- src/main/scala/com/evolution/datagen/GeneratorApp.scala
+++ src/main/scala/com/evolution/datagen/GeneratorApp.scala
@@ -7,6 +7,11 @@ import com.evolution.datagen.generator.{Gen, RateLimiter}
 import com.evolution.datagen.model.DataModel.ObjectField
 import com.evolution.datagen.model.SpecModel.GeneratorSpec
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
 object GeneratorApp extends IOApp {
 
   import cats.syntax.parallel._
@@ -14,19 +19,22 @@ object GeneratorApp extends IOApp {
   import io.circe.syntax._
 
   def process: IO[Unit] = {
-    val eventsNumber = 1000000
-    for {
-      dataSpec    <- GeneratorSpec.of("data_spec_example.yaml")
-      clock_start <- timer.clock.monotonic(TimeUnit.MILLISECONDS)
-      semaphore   <- Semaphore[IO](eventsNumber)
+    val eventsNumber = 10000
+    (for {
+      dataSpec          <- GeneratorSpec.of("data_spec_example.yaml")
+      clockStart        <- timer.clock.realTime(TimeUnit.MILLISECONDS)
+      semaphore         <- Semaphore[IO](eventsNumber)
       rateLimitedFunction = RateLimiter.of(semaphore, () => IO.delay(Gen.from(dataSpec).asInstanceOf[ObjectField].asJson.noSpaces))
-      allResults  <- Range(0, eventsNumber).toList.parTraverse(_ => rateLimitedFunction)
-      clock_end   <- timer.clock.monotonic(TimeUnit.MILLISECONDS)
-      _           <- IO(println(s"sample of data ${allResults.take(10).mkString(System.lineSeparator)}."))
-      _           <- IO(println(s"generation of ${allResults.size} taken ${clock_end - clock_start} millisec"))
-      _           <- IO(println(s"Size is ${allResults.size}"))
-    } yield ()
-
+      allResults        <- Range(0, eventsNumber).toList.parTraverse(_ => rateLimitedFunction)
+      clockGenEnd       <- timer.clock.realTime(TimeUnit.MILLISECONDS)
+      _                 <- IO(println(s"sample of data ${allResults.take(1).mkString(System.lineSeparator)}."))
+      _                 <- IO(println(s"generation of ${allResults.size} taken ${clockGenEnd - clockStart} milliseconds"))
+      clockWriteFileEnd <- timer.clock.realTime(TimeUnit.MILLISECONDS)
+      eventsAsFile      <- IO(allResults.mkString(System.lineSeparator).getBytes(StandardCharsets.UTF_8))
+      fileName = s"target/tmp/${Instant.ofEpochMilli(clockStart)}-events.json"
+      _                 <- IO(println(s"writing of $fileName has taken ${clockWriteFileEnd - clockGenEnd} milliseconds"))
+      _                 <- IO(Files.write(Paths.get(fileName), eventsAsFile))
+    } yield ()) *> IO.suspend(process)
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
